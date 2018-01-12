245cfb807acd9bbc680dc6c530b653fdb0f20785
Navtej Singh <nsingh20@,mail.greenriver.edu>
2018-01-12 17:48:18 -0500

    new files

--- index.php
+++ index.php
@@ -5,14 +5,34 @@ require_once ('vendor/autoload.php');
 //create an instance of the base class
 $f3 = Base::instance();
 
+//Set debug level
+$f3->set('DEBUG',3);
+
 //define a default route
 $f3->route('GET /',
     function() {
        echo'<h1>Routing Demo</h1>';
-
       }
 );
+//define a Page1 route
+$f3->route('GET /page1',
+    function() {
+        echo'<h1>This is page1</h1>';
+    }
+);
 
+//define a Page2 route
+$f3->route('GET /page2',
+    function() {
+        echo'<h1>Page2</h1>';
+    }
+);
+//define a page-sub route
+$f3->route('GET /page1/subpage-a',
+    function() {
+        echo'<h1>Subpage-a</h1>';
+    }
+);
 //run fat free
 $f3->run();
 ?>
