fa672a00cd7602495c3d58c8c8323fe73523885a
Navtej Singh <nsingh20@,mail.greenriver.edu>
2018-01-12 17:18:28 -0500

    First commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+//require the autolaod file
+require_once ('vendor/autoload.php');
+
+//create an instance of the base class
+$f3 = Base::instance();
+
+//define a default route
+$f3->route('GET /',
+    function() {
+       echo'<h1>Routing Demo</h1>';
+
+      }
+);
+
+//run fat free
+$f3->run();
+?>
