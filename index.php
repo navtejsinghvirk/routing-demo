02072476d4dd507cb74c147250b05b35e492b966
navtej singh <nsingh20@mail.greenriver.edu>
2018-01-16 17:48:42 -0500

    add session

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php
+session_start();
+//error_reporting(E_ALL);
+//ini_set("display_errors",1);
+
 //require the autolaod file
 require_once ('vendor/autoload.php');
 
@@ -33,6 +37,56 @@ $f3->route('GET /page1/subpage-a',
         echo'<h1>Subpage-a</h1>';
     }
 );
+//define a template route
+$f3->route('GET /jewelry/rings/toe-rings',
+    function() {
+      echo'<h1>Toe Rings</h1>';
+      //  $template = new Template();
+     // echo $template->render('views/toe-rings.html');
+    }
+);
+
+$f3->route('GET /hello/@name',function  ($f3,$params) {
+      //  $name = $params['name'];
+       // echo "<h1>Hello,$name</h1>";
+        $f3-> set('name',$params['name']);
+        $template = new Template();
+        echo $template->render('views/hello.html');
+
+    });
+
+$f3->route('GET /hi/@first/@last',function  ($f3,$params) {
+    //  $name = $params['name'];
+    // echo "<h1>Hello,$name</h1>";
+    $f3-> set('first',$params['first']);
+    $f3-> set('last',$params['last']);
+    $f3-> set('message','hi');
+
+    $_SESSION['first'] = $f3-> get('first');
+    $_SESSION['last'] = $f3-> get('last');
+    $template = new Template();
+    echo $template->render('views/hi.html');
+
+});
+$f3-> route('GET /hi-again',function($f3,$params){
+    echo 'Hi again, '.$_SESSIONcd['first'];
+});
+$f3->route('GET /language/@lang', function($f3,$params){
+    switch ($params['lang']){
+        case 'swahili';
+        echo 'Jumbo'; break;
+        case 'spanish';
+            echo 'Hola'; break;
+        case 'Privet';
+            echo 'farsi'; break;
+            case'french';
+            $f3->reroute('/');
+        default:
+            $f3->error(404);
+
+    }
+});
+
 //run fat free
 $f3->run();
 ?>
